5b290519798be5dffc4de7ba2f7b784895484d84
Backl1ght <1229309527@qq.com>
2022-11-09 21:59:22 +0800

    feat(hira/graph): add dijkstra and spfa

---
+++ hira/graph/shortest_path/dijkstra.h
@@ -0,0 +1,47 @@
+#ifndef HIRA_GRAPH_SHORTEST_PATH_DIJKSTRA_H
+#define HIRA_GRAPH_SHORTEST_PATH_DIJKSTRA_H
+
+#include "hira/common_header.h"
+
+namespace hira {
+
+template <typename DistanceType,
+          typename Comp = std::greater<>,
+          typename Edge = std::pair<DistanceType, i32>,
+          typename Node = std::pair<DistanceType, i32>>
+std::vector<DistanceType> Dijkstra(const std::vector<std::vector<Edge>>& g,
+                                   const std::vector<i32>& s) {
+  const DistanceType INF = std::numeric_limits<DistanceType>::max();
+  const i32 n = g.size();
+  const Comp comp;
+
+  std::vector<DistanceType> dis(n, INF);
+  std::vector<bool> vis(n, false);
+
+  std::priority_queue<Node, std::vector<Node>, Comp> q;
+  for (i32 u : s) {
+    dis[u] = 0;
+    q.push(Node(dis[u], u));
+  }
+  while (!q.empty()) {
+    auto [c, u] = q.top();
+    q.pop();
+
+    if (vis[u])
+      continue;
+    vis[u] = true;
+
+    for (auto [w, v] : g[u]) {
+      if (comp(dis[v], c + w)) {
+        dis[v] = c + w;
+        q.push(Node(dis[v], v));
+      }
+    }
+  }
+
+  return dis;
+}
+
+}  // namespace hira
+
+#endif  // HIRA_GRAPH_SHORTEST_PATH_DIJKSTRA_H
---
+++ hira/graph/shortest_path/spfa.h
@@ -0,0 +1,47 @@
+#ifndef HIRA_GRAPH_SHORTEST_PATH_SPFA_H
+#define HIRA_GRAPH_SHORTEST_PATH_SPFA_H
+
+#include "hira/common_header.h"
+
+namespace hira {
+
+template <typename DistanceType,
+          typename Comp = std::greater<>,
+          typename Edge = std::pair<DistanceType, i32>>
+std::vector<DistanceType> SPFA(const std::vector<std::vector<Edge>>& g,
+                               const std::vector<i32>& s) {
+  const DistanceType INF = std::numeric_limits<DistanceType>::max();
+  const i32 n = g.size();
+  const Comp comp;
+
+  std::vector<DistanceType> dis(n, INF);
+  std::vector<bool> inq(n, false);
+
+  std::queue<i32> q;
+  for (i32 u : s) {
+    dis[u] = 0;
+    inq[u] = true;
+    q.push(u);
+  }
+  while (!q.empty()) {
+    i32 u = q.front();
+    q.pop();
+    inq[u] = false;
+
+    for (auto [w, v] : g[u]) {
+      if (comp(dis[v], dis[u] + w)) {
+        dis[v] = dis[u] + w;
+        if (inq[v] == false) {
+          inq[v] = true;
+          q.push(v);
+        }
+      }
+    }
+  }
+
+  return dis;
+}
+
+}  // namespace hira
+
+#endif  // HIRA_GRAPH_SHORTEST_PATH_SPFA_H
